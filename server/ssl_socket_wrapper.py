9ac9ea9a3f0dbf759e0da836a9dc58378308f895
William Budington <bill.budington@gmail.com>
2013-01-06 16:09:41 -0500

    writing ssl part into ssl socket wrapper

--- server/ssl_socket_wrapper.py
+++ server/ssl_socket_wrapper.py
@@ -7,3 +7,19 @@ class SSLSocketWrapper():
 
   def listen(self):
     self.bindsocket.listen(0)
+    while True:
+      newsocket, fromaddr = self.bindsocket.accept()
+      connstream = ssl.wrap_socket(newsocket,
+        server_side=True,
+        certfile="./ssl/server.crt",
+        keyfile="./ssl/server.key",
+        ssl_version=ssl.PROTOCOL_TLSv1)
+      try:
+        data = connstream.read()
+        while data:
+          data = connstream.read()
+      except ssl.SSLError, e:
+        pass
+      finally:
+        connstream.shutdown(socket.SHUT_RDWR)
+        connstream.close()

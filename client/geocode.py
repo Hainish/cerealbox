1100dc7f9f447f8335ab830d4eb5a4a0663e4715
William Budington <bill.budington@gmail.com>
2013-01-13 01:23:45 -0800

    handle exception for ip not found (geocode)

--- client/geocode.py
+++ client/geocode.py
@@ -4,6 +4,9 @@ from incf.countryutils import transformations
 gi = pygeoip.GeoIP('/usr/share/GeoIP/GeoIP.dat')
 
 def lookup(ip):
-  country_code = gi.country_code_by_addr(ip)
-  continent = transformations.cca_to_ctca2(country_code)
+  try:
+    country_code = gi.country_code_by_addr(ip)
+    continent = transformations.cca_to_ctca2(country_code)
+  except Exception, e:
+    return (None, None)
   return (country_code, continent)
